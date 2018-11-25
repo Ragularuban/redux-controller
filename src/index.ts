1023632c26815d1ecd23922fb17a33b574c99450
Ruban <ragularuban@hotmail.com>
2018-11-25 18:25:14 +0530

    exported missing functions and configured travis ci

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-// Export Redux Controller
-export { ReduxController, ReduxControllerBase } from "./redux-controller";
+// Export Redux Controller Registry
+export { ReduxControllerRegistry } from "./redux-controller.registry";
+
+// Export Redux Controller and core functions
+export { ReduxController, ReduxControllerBase, ReduxAsyncAction, ReduxWatch, ReduxAction, CommitFunction } from "./redux-controller";
 
 // Export Redux Controller Helpers
 export { GetController, GetSafely } from "./helpers";
-
 
 // Export React Helpers
 export { AutoUnsubscribe } from "./react-helpers";
