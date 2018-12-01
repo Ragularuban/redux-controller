7189feb7bb7a279b4121313838bc2de491b30ab2
Ruban <ragularuban@hotmail.com>
2018-12-02 01:09:20 +0530

    *

--- src/providers.ts
+++ src/providers.ts
@@ -1,26 +1,23 @@
-import createEngineReactNative from 'redux-storage-engine-reactnativeasyncstorage';
-import createEngineLocalStorage from 'redux-storage-engine-localstorage';
-
 export type Environnement = "REACT_NATIVE" | "ANGULAR" | "NODE" | "REACT";
 
 export class Providers {
     static getCreateEngine(env: Environnement) {
         switch (env) {
             case "REACT_NATIVE": {
-                return createEngineReactNative;
+                return require("redux-storage-engine-reactnativeasyncstorage");
             }
             case "REACT": {
-                return createEngineLocalStorage;
+                return require("redux-storage-engine-localstorage");
             }
             case "ANGULAR": {
-                return createEngineLocalStorage;
+                return require("redux-storage-engine-localstorage");
             }
             case "NODE": {
                 // Todo: Need to Add Local Storage
                 throw new Error("Node is not yet supported");
             }
             default: {
-                return createEngineLocalStorage;
+                return require("redux-storage-engine-localstorage");
             }
         }
 
